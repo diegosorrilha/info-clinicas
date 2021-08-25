c37c22b7e8fab48a2aa317148948baf68b5d70b9
Igor Sorrilha <igor.sorrilha@gmail.com>
2021-08-24 21:03:45 -0300

    cria models.py base com informações basicas do paciente

--- info_clinicas/pacientes/models.py
+++ info_clinicas/pacientes/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+class Paciente(models.Model):
+    
+    nome_completo = models.CharField(
+        verbose_name="Nome completo",
+        max_length=194
+    )
+
+    data_nascimento = models.DateField(
+        verbose_name="Data de nascimento",
+        auto_now=False,
+        auto_now_add=False,
+    )
+    
+    telefone = models.CharField(
+        verbose_name="Telefone",
+        max_length=11,
+    )
+
+    endereço = models.CharField(
+        verbose_name="Endereço (rua, número e bairro)",
+        max_length=194,
+    )
+
+    cidade = models.CharField(
+        verbose_name="Cidade",
+        max_length=194,
+    )
+
+    estado = models.CharField(
+        verbose_name="Estado",
+        max_length=194,
+    )
+
+    email = models.EmailField(
+        verbose_name="E-mail",
+        max_length=194,
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Paciente"
+        verbose_name_plural = "Pacientes"
+        db_table = "paciente"
+    
+    def __str__(self):
+        return self.nome_completo
