4d5a287f3da0003758a6447c3baa4c43db1e1840
Igor Sorrilha <igor.sorrilha@gmail.com>
2021-08-26 15:48:57 -0300

    templates criados e configurados

--- info_clinicas/core/views.py
+++ info_clinicas/core/views.py
@@ -3,7 +3,5 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse(
-        '<h1>Bem vindo ao Info Clinicas :)</h1>'
-        '<p>Agora com ambiente de Desenvolvimento</p>'
-    )
+    
+    return render(request, 'core.html')
--- info_clinicas/settings.py
+++ info_clinicas/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'info_clinicas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, "static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
