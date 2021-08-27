8ff9b10c214218646b38d0da0fc9a8d822bc861c
Diego Sorrilha <diego@sorrilha.net>
2021-08-27 18:09:03 -0300

    cria view basica para cadastrar clinica

--- info_clinicas/clinicas/views.py
+++ info_clinicas/clinicas/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def cadastrar_clinica(request):
+
+    return HttpResponse('<h1>Cadastrar Clínica</h1>')
