9ba8d78b83a4489d6eae7a9e792c55a4b6e7e87c
Igor Sorrilha <igor.sorrilha@gmail.com>
2021-08-27 21:27:26 -0300

    cria uma view para exibição do formulário de cadastro da pacientes

--- info_clinicas/pacientes/views.py
+++ info_clinicas/pacientes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def cadastrar_paciente(request):
+
+    context = {}
+    
+    return render(request, 'cadastrar_paciente.html', context)
