cbe55408b1160616feab1dc544c995a6eb95368c
Thiago Sorrilha <thiago_sorrilha@hotmail.com>
2021-08-28 00:58:32 -0300

    ajuste no arquivo urls

--- info_clinicas/urls.py
+++ info_clinicas/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from especialidade.views import cadastrar_especialidade
+from info_clinicas.especialidade.views import cadastrar_especialidade
 from info_clinicas.core.views import home
 
 urlpatterns = [
