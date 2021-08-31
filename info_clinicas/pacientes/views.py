01e7288bb06a6b892411b2a0c74d0ad094fb7517
Igor Sorrilha <igor.sorrilha@gmail.com>
2021-08-31 10:55:35 -0300

    funcionalidade criada pra salvamento de informações no banco de dados

--- info_clinicas/pacientes/views.py
+++ info_clinicas/pacientes/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from info_clinicas.pacientes.models import Paciente
 
 def cadastrar_paciente(request):
+    if request.method == 'POST':
+        n_c = request.POST['nome-completo'] 
+        tel = request.POST['telefone']
+        email = request.POST['email'] 
+        endereço = request.POST['endereço']
+        cidade = request.POST['cidade'] 
+        estado = request.POST['estado'] 
+        sexo = request.POST['sexo'] 
+        datanascimento = request.POST['data-nascimento']
+
+        paciente = Paciente(
+            nome_completo=n_c.lower(),
+            telefone=tel,
+            email=email,
+            endereço=endereço.lower(),
+            cidade=cidade.lower(),
+            estado=estado.lower(),
+            sexo=sexo,
+            data_nascimento=datanascimento,
+        )
+
+        paciente.save()
 
     context = {}
     
