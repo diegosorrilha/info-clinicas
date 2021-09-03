328832e53830012b125b116b5d872fa644d91bfe
Thiago Sorrilha <thiago_sorrilha@hotmail.com>
2021-09-03 00:04:22 -0300

    Ajuste formulario e input data de inclusao

--- info_clinicas/especialidade/models.py
+++ info_clinicas/especialidade/models.py
@@ -6,4 +6,10 @@ class Especialidade(models.Model):
     Nome_especialidade = models.CharField(
         verbose_name="Especialidade",
         max_length=194,
+    ),
+
+    Data_inclusao = models.DateField(
+        verbose_name='Data Inclusao',
+        auto_now_add=False,
+        auto_now=False,
     )
--- info_clinicas/especialidade/views.py
+++ info_clinicas/especialidade/views.py
@@ -1,3 +1,5 @@
+
+from info_clinicas.especialidade.models import Especialidade
 from django.shortcuts import render
 # from django.http import HttpResponse
 
@@ -5,4 +7,10 @@ from django.shortcuts import render
 def cadastrar_especialidade(request):
 
     # return HttpResponse('<h1>Cadastrar Especialidades</h1>')
+    if request.method == "POST":
+        nomeespecialidade = request.POST['Nome_especialidade']
+        datainclusao = request.POST['data-inclusao']
+
+        print(nomeespecialidade, datainclusao)
+
     return render(request, 'especialidade.html')
