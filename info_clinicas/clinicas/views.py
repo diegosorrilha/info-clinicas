0f326d5752b4d77a9092ec5650d1cf9b2aa4eb81
Diego Sorrilha <diego@sorrilha.net>
2021-09-05 15:10:26 -0300

    implementa formulário de cadastro de Cĺinica com padrão

--- info_clinicas/clinicas/views.py
+++ info_clinicas/clinicas/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 
 
 def cadastrar_clinica(request):
+    if request.method == 'POST':
+        nome = request.POST['nome_clinica']
+        cnpj = request.POST['cpnj']
+        site = request.POST['site']
+        endereco = request.POST['endereco']
+        cidade = request.POST['cidade']
+        estado = request.POST['estado']
+        telefone = request.POST['telefone']
+        responsavel = request.POST['responsavel']
+
+        print(nome, cnpj, site, endereco, cidade, estado, telefone, responsavel)
 
     return render(request, 'cadastrar-clinica.html')
