115721cf679a1f018f3c85ed9a04d599c55cf6a5
Thiago Sorrilha <thiago_sorrilha@hotmail.com>
2021-09-21 16:09:55 -0300

    Ajustes ref. aos pitacos do Tchu-tchu

--- info_clinicas/especialidade/management/commands/inserir_especialidade.py
+++ info_clinicas/especialidade/management/commands/inserir_especialidade.py
@@ -8,12 +8,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        cria = Especialidade(nome_especialidade='Acupuntura')
-        cria.save()
+        lista = ['Acupuntura', 'Alergia e Imunologia', 'Anestesiologia', 'Angiologia', 'Cancerologia', 'Cardiologia', 'Cirurgia Cardiovascular', 'Cirurgia da Mão', 'Cirurgia de Cabeça e Pescoço', 'Cirurgia do Aparelho Digestivo', 'Cirurgia Geral', 'Cirurgia Pediátrica', 'Cirurgia Plástica', 'Cirurgia Torácica', 'Cirurgia Vascular', 'Clínica Médica', 'Coloproctologia', 'Dermatologia', 'Endocrinologia e Metabologia', 'Endoscopia', 'Gastroenterologia', 'Genética Médica', 'Geriatria', 'Ginecologia e Obstetrícia', 'Hematologia e Hemoterapia', 'Homeopatia', 'Infectologia',
+                 'Mastologia', 'Medicina de Família e Comunidade', 'Medicina do Trabalho', 'Medicina de Tráfego', 'Medicina Esportiva', 'Medicina Física e Reabilitação', 'Medicina Intensiva', 'Medicina Legal e Perícia Médica', 'Medicina Nuclear', 'Medicina Preventiva e Social', 'Nefrologia', 'Neurocirurgia', 'Neurologia', 'Nutrologia', 'Oftalmologia', 'Ortopedia e Traumatologia', 'Otorrinolaringologia', 'Patologia', 'Patologia Clínica/Medicina Laboratorial', 'Pediatria', 'Pneumologia', 'Psiquiatria', 'Radiologia e Diagnóstico por Imagem', 'Radioterapia', 'Reumatologia', 'Urologia']
 
-    lista = ['Alergia e Imunologia', 'Anestesiologia', 'Angiologia', 'Cancerologia', 'Cardiologia', 'Cirurgia Cardiovascular', 'Cirurgia da Mão', 'Cirurgia de Cabeça e Pescoço', 'Cirurgia do Aparelho Digestivo', 'Cirurgia Geral', 'Cirurgia Pediátrica', 'Cirurgia Plástica', 'Cirurgia Torácica', 'Cirurgia Vascular', 'Clínica Médica', 'Coloproctologia', 'Dermatologia', 'Endocrinologia e Metabologia', 'Endoscopia', 'Gastroenterologia', 'Genética Médica', 'Geriatria', 'Ginecologia e Obstetrícia', 'Hematologia e Hemoterapia', 'Homeopatia', 'Infectologia',
-             'Mastologia', 'Medicina de Família e Comunidade', 'Medicina do Trabalho', 'Medicina de Tráfego', 'Medicina Esportiva', 'Medicina Física e Reabilitação', 'Medicina Intensiva', 'Medicina Legal e Perícia Médica', 'Medicina Nuclear', 'Medicina Preventiva e Social', 'Nefrologia', 'Neurocirurgia', 'Neurologia', 'Nutrologia', 'Oftalmologia', 'Ortopedia e Traumatologia', 'Otorrinolaringologia', 'Patologia', 'Patologia Clínica/Medicina Laboratorial', 'Pediatria', 'Pneumologia', 'Psiquiatria', 'Radiologia e Diagnóstico por Imagem', 'Radioterapia', 'Reumatologia', 'Urologia']
-
-    for i in lista:
-        especialidade = Especialidade(nome_especialidade=i)
-        especialidade.save()
+        for i in lista:
+            especialidade = Especialidade(nome_especialidade=i)
+            especialidade.save()
--- info_clinicas/especialidade/views.py
+++ info_clinicas/especialidade/views.py
@@ -1,12 +1,10 @@
 
 from info_clinicas.especialidade.models import Especialidade
 from django.shortcuts import render
-# from django.http import HttpResponse
 
 
 def cadastrar_especialidade(request):
 
-    # return HttpResponse('<h1>Cadastrar Especialidades</h1>')
     if request.method == "POST":
         nomeespecialidade = request.POST['nome_especialidade']
         datainclusao = request.POST['data_inclusao']
