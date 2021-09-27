a8f81f5bf4f9bde71e33198a9b9325799ae64a36
Igor Sorrilha <igor.sorrilha@gmail.com>
2021-09-27 20:25:53 -0300

    criação do models de agendamento e suas migrations

--- info_clinicas/agendamento/models.py
+++ info_clinicas/agendamento/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Agendamento(models.Model):
+
+    STATUS_AGENDAMENTO = [
+        ("AGENDADO", "Agendado"),
+        ("CONSULTADO", "Consultado"),
+        ("CANCELADO", "Cancelado"),
+    ]
+
+    status = models.CharField(
+        verbose_name="Status de agendamento",
+        max_length=126,
+        choices=STATUS_AGENDAMENTO,
+        default="AGENDADO",
+    )
+    
+    agendado_para = models.ForeignKey(
+        "medicos.Disponibilidade",
+        on_delete=models.PROTECT,
+    )
+
+    medico = models.ForeignKey(
+        "medicos.Medicos",
+        on_delete=models.PROTECT,
+    )
+    
+    paciente = models.ForeignKey(
+        "pacientes.Paciente",
+        on_delete=models.PROTECT,
+    )
+
+    class Meta:
+        verbose_name = "Agendamento"
+        verbose_name_plural = "Agendamentos"
+        db_table = "agendamento"
