5476a1f557ce0a9217e7b149a8808547e2fb6cea
Igor Sorrilha <igor.sorrilha@gmail.com>
2021-09-27 19:50:00 -0300

    criação da tabela disponibilidade dentro do app medicos

--- info_clinicas/medicos/models.py
+++ info_clinicas/medicos/models.py
@@ -14,7 +14,7 @@ class Medicos(models.Model):
     sexo = models.CharField(verbose_name="Sexo", max_length=9, choices=SEXO_ESCOLHA, default=None)
     cpf = models.CharField(verbose_name="CPF", max_length=11)
     email = models.EmailField(verbose_name="E-mail", max_length=194, blank=True, null=True)
-    data_nascimento = models.DateField(verbose_name="Data de nascimento")
+    data_nascimento = models.DateField(verbose_name="Data de nascimento", auto_now=False, auto_now_add=False)
     telefone = models.CharField(verbose_name="Telefone", max_length=11)
     endereco = models.CharField(verbose_name="Endereço", max_length=194)
     bairro = models.CharField(verbose_name="Bairro", max_length=30)
@@ -22,7 +22,13 @@ class Medicos(models.Model):
     pais = models.CharField(verbose_name="País", max_length=12)
     CEP = models.CharField(verbose_name="CEP", max_length=9)
     CRM = models.CharField(verbose_name="CRM", max_length=7)
-
+    
+class Disponibilidade(models.Model):
+    
+    disponivel = models.BooleanField(default=True)
+    medico = models.ForeignKey("medicos.Medicos", on_delete=models.PROTECT)
+    data = models.DateField(verbose_name="Data disponível", auto_now=False, auto_now_add=False)
+    hora = models.DateTimeField(verbose_name="Horário disponível", auto_now=False, auto_now_add=False)
 
     class Meta:
         verbose_name = "Médico"
