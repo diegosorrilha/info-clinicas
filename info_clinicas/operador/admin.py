39779e7f716e1423aced40c6bae6a26d0281ca43
Thiago Sorrilha <thiago_sorrilha@hotmail.com>
2021-10-05 22:05:57 -0300

    Criação do App operador

---
+++ info_clinicas/operador/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from info_clinicas.operador.models import Operador
+
+admin.site.register(Operador)
---
+++ info_clinicas/operador/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class OperadorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'operador'
+    name = 'info_clinicas.operador'
